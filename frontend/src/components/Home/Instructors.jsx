c6d2dccfa16f33dfed2502191c1e339b54113895
MohamedEBR <110148669+MohamedEBR@users.noreply.github.com>
2024-03-13 16:03:43 -0400

    Updated Pricing.jsx

--- frontend/src/components/Home/Instructors.jsx
+++ frontend/src/components/Home/Instructors.jsx
@@ -13,8 +13,6 @@ const Instructors = () => {
         justifyContent: "center",
         height: "100%",
         textAlign: { xs: "center", sm: "center", md: "center", lg: "start" },
-        pb: 5,
-        mb: 5,
         px: "7%",
       }}
     >
@@ -107,63 +105,81 @@ const Instructors = () => {
         </Box>
       </Box>
       <Box
-  sx={{
-    width: "100%",
-    display: "flex",
-    flexDirection: { xs: "column", sm: "column", md: "column", lg: "row" }, // Only change here (md: "row")
-    justifyContent: "space-between",
-    alignItems: { xs: "center", sm: "center", md: "center", lg: "flex-start" }, // Minor adjustment (md: "flex-start")
-    mb: 20,
-  }}
->
-  <Box
-    component="img"
-    sx={{
-      width: { xs: "100%", sm: "100%", md: "60%", lg: "50%" },
-      height: "500px",
-      objectFit: "none",
-      objectPosition: "center",
-      position: "relative",
-      borderRadius: "0% 0% 0% 0% / 0% 0% 0% 0%",
-      boxShadow: "20px 20px rgba(0,0,0,.15)",
-      mb: { xs: 5, sm: 5, md: 5, lg: 0 },
-      mr: 5,
-      transition: "all .4s ease",
-      ":hover": {
-        borderRadius: "0% 0% 50% 50% / 0% 0% 5% 5%",
-        boxShadow: "10px 10px rgba(0,0,0,.25)",
-      },
-    }}
-    alt="Logo"
-    src={images.karimNats2medals}
-  />
-  <Box
-    component="div"
-    sx={{
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "start",
-      alignItems: "start",
-      width: { xs: "100%", sm: "100%", md: "100%", lg: "50%" }, // Adjust width on medium screens
-    }}
-  >
-    <Typography variant="h3" sx={{ fontFamily: '"Saira Semi Condensed"', mb: 3, width: "100%" }}>
-      Sensei Karim Ebraheem
-    </Typography>
-    <Typography
-      variant="subtitle1"
-      sx={{ fontFamily: '"Saira Semi Condensed"', pt: 2, fontSize: 20, width: "100%" }}
-    >
-     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        sx={{
+          width: "100%",
+          display: "flex",
+          flexDirection: {
+            xs: "column",
+            sm: "column",
+            md: "column",
+            lg: "row",
+          }, // Only change here (md: "row")
+          justifyContent: "space-between",
+          alignItems: {
+            xs: "center",
+            sm: "center",
+            md: "center",
+            lg: "flex-start",
+          }, // Minor adjustment (md: "flex-start")
+          mb: 20,
+        }}
+      >
+        <Box
+          component="img"
+          sx={{
+            width: { xs: "100%", sm: "100%", md: "60%", lg: "50%" },
+            height: "500px",
+            objectFit: "none",
+            objectPosition: "center",
+            position: "relative",
+            borderRadius: "0% 0% 0% 0% / 0% 0% 0% 0%",
+            boxShadow: "20px 20px rgba(0,0,0,.15)",
+            mb: { xs: 5, sm: 5, md: 5, lg: 0 },
+            mr: 5,
+            transition: "all .4s ease",
+            ":hover": {
+              borderRadius: "0% 0% 50% 50% / 0% 0% 5% 5%",
+              boxShadow: "10px 10px rgba(0,0,0,.25)",
+            },
+          }}
+          alt="Logo"
+          src={images.karimNats2medals}
+        />
+        <Box
+          component="div"
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "start",
+            alignItems: "start",
+            width: { xs: "100%", sm: "100%", md: "100%", lg: "50%" }, // Adjust width on medium screens
+          }}
+        >
+          <Typography
+            variant="h3"
+            sx={{ fontFamily: '"Saira Semi Condensed"', mb: 3, width: "100%" }}
+          >
+            Sensei Karim Ebraheem
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            sx={{
+              fontFamily: '"Saira Semi Condensed"',
+              pt: 2,
+              fontSize: 20,
+              width: "100%",
+            }}
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
             aliquip ex ea commodo consequat. Duis aute irure dolor in
             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
             culpa qui officia deserunt mollit anim id est laborum.
-    </Typography>
-  </Box>
-</Box>
+          </Typography>
+        </Box>
+      </Box>
       <Box
         sx={{
           width: "100%",
--- frontend/src/components/Home/Pricing.jsx
+++ frontend/src/components/Home/Pricing.jsx
@@ -1,8 +1,327 @@
+import * as React from "react";
+import Box from "@mui/joy/Box";
+import Button from "@mui/joy/Button";
+import Card from "@mui/joy/Card";
+import CardActions from "@mui/joy/CardActions";
+import Chip from "@mui/joy/Chip";
+import Divider from "@mui/joy/Divider";
+import List from "@mui/joy/List";
+import ListItem from "@mui/joy/ListItem";
+import ListItemDecorator from "@mui/joy/ListItemDecorator";
+import Typography from "@mui/material/Typography";
+import Check from "@mui/icons-material/Check";
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
+import { CustomBox } from "../shared/CustomBox";
 
-const Pricing = () => {
+export default function Pricing() {
   return (
-    <div>Pricing</div>
-  )
-}
+    <CustomBox
+      sx={{
+        p: "3% 7%",
+        my: 10,
+        boxShadow: 3,
+        display: "flex",
+        flexDirection: "column",
 
-export default Pricing
+        textAlign: {
+          xs: "center",
+          sm: "center",
+          md: "center",
+          lg: "start",
+        },
+      }}
+    >
+      <Typography
+        variant="h6"
+        sx={{
+          fontFamily: '"Saira Semi Condensed"',
+          color: "#fff",
+          my: 3,
+        }}
+      >
+        Pricing
+      </Typography>
+      <Typography
+        variant="h4"
+        sx={{
+          fontFamily: '"Saira Semi Condensed"',
+          color: "#fff",
+          mt: 3,
+          mb: 5,
+        }}
+      >
+        We offer flexible membership plans to suit your needs and budget.
+      </Typography>
+      <Box
+        component="div"
+        sx={{
+          display: "grid",
+          gridTemplateColumns: {
+            xs: "repeat(auto-fill, minmax(275px, 1fr))",
+            sm: "repeat(auto-fill, minmax(275px, 1fr))",
+            md: "repeat(auto-fill, minmax(300px, 1fr))",
+            lg :"repeat(auto-fill, minmax(350px, 1fr))"}, // Adjust min-width as needed
+          justifyContent: "space-between",
+          alignItems: "center",
+          gap: 2,
+        }}
+      >
+        <Card
+          size="lg"
+          variant="outlined"
+          sx={{
+            mx: 2,
+          }}
+        >
+          <Chip size="sm" variant="outlined" color="neutral">
+            B
+          </Chip>
+          <Typography
+            variant="h3"
+            sx={{ fontFamily: '"Saira Semi Condensed"' }}
+          >
+            Beginner
+          </Typography>
+          <Divider inset="none" />
+          <List size="sm" sx={{ mx: "calc(-1 * var(--ListItem-paddingX))" }}>
+            <ListItem>
+              <ListItemDecorator>
+                <Check />
+              </ListItemDecorator>
+              2 hr/week
+            </ListItem>
+            <ListItem>
+              <ListItemDecorator>
+                <Check />
+              </ListItemDecorator>
+              Financial Analytics
+            </ListItem>
+            <ListItem>
+              <ListItemDecorator>
+                <Check />
+              </ListItemDecorator>
+              Checking Account
+            </ListItem>
+            <ListItem>
+              <ListItemDecorator>
+                <Check />
+              </ListItemDecorator>
+              API Integration
+            </ListItem>
+          </List>
+          <Divider inset="none" />
+          <CardActions>
+            <Typography
+              variant="h6"
+              sx={{ fontFamily: '"Saira Semi Condensed"', mr: "auto" }}
+            >
+              $85{" "}
+              <Typography variant="body2" color="text.secondary">
+                / month
+              </Typography>
+            </Typography>
+            <Button
+              variant="soft"
+              color="neutral"
+              endIcon={<KeyboardArrowRight />}
+            >
+              Start now
+            </Button>
+          </CardActions>
+        </Card>
+        <Card
+          size="lg"
+          variant="outlined"
+          sx={{
+            mx: 2,
+            
+          }}
+        >
+          <Chip size="sm" variant="outlined" color="neutral">
+            A
+          </Chip>
+          <Typography
+            variant="h3"
+            sx={{ fontFamily: '"Saira Semi Condensed"' }}
+          >
+            Beginner
+          </Typography>
+          <Divider inset="none" />
+          <List size="sm" sx={{ mx: "calc(-1 * var(--ListItem-paddingX))" }}>
+            <ListItem>
+              <ListItemDecorator>
+                <Check />
+              </ListItemDecorator>
+              2 hr/week
+            </ListItem>
+            <ListItem>
+              <ListItemDecorator>
+                <Check />
+              </ListItemDecorator>
+              Financial Analytics
+            </ListItem>
+            <ListItem>
+              <ListItemDecorator>
+                <Check />
+              </ListItemDecorator>
+              Checking Account
+            </ListItem>
+            <ListItem>
+              <ListItemDecorator>
+                <Check />
+              </ListItemDecorator>
+              API Integration
+            </ListItem>
+          </List>
+          <Divider inset="none" />
+          <CardActions>
+            <Typography
+              variant="h6"
+              sx={{ fontFamily: '"Saira Semi Condensed"', mr: "auto" }}
+            >
+              $105{" "}
+              <Typography variant="body2" color="text.secondary">
+                / month
+              </Typography>
+            </Typography>
+            <Button
+              variant="soft"
+              color="neutral"
+              endIcon={<KeyboardArrowRight />}
+            >
+              Start now
+            </Button>
+          </CardActions>
+        </Card>
+        <Card
+          size="lg"
+          variant="outlined"
+          sx={{
+            mx: 2,
+            
+          }}
+        >
+          <Chip size="sm" variant="outlined" color="neutral">
+            Basic
+          </Chip>
+          <Typography
+            variant="h3"
+            sx={{ fontFamily: '"Saira Semi Condensed"' }}
+          >
+            Elite
+          </Typography>
+          <Divider inset="none" />
+          <List size="sm" sx={{ mx: "calc(-1 * var(--ListItem-paddingX))" }}>
+            <ListItem>
+              <ListItemDecorator>
+                <Check />
+              </ListItemDecorator>
+              2 hr/week
+            </ListItem>
+            <ListItem>
+              <ListItemDecorator>
+                <Check />
+              </ListItemDecorator>
+              Financial Analytics
+            </ListItem>
+            <ListItem>
+              <ListItemDecorator>
+                <Check />
+              </ListItemDecorator>
+              Checking Account
+            </ListItem>
+            <ListItem>
+              <ListItemDecorator>
+                <Check />
+              </ListItemDecorator>
+              API Integration
+            </ListItem>
+          </List>
+          <Divider inset="none" />
+          <CardActions>
+            <Typography
+              variant="h6"
+              sx={{ fontFamily: '"Saira Semi Condensed"', mr: "auto" }}
+            >
+              $150{" "}
+              <Typography variant="body2" color="text.secondary">
+                / month
+              </Typography>
+            </Typography>
+            <Button
+              variant="soft"
+              color="neutral"
+              endIcon={<KeyboardArrowRight />}
+            >
+              Start now
+            </Button>
+          </CardActions>
+        </Card>
+        <Card
+          size="lg"
+          variant="outlined"
+          sx={{
+            mx: 2,
+            
+          }}
+        >
+          <Chip size="sm" variant="outlined" color="neutral">
+            Basic
+          </Chip>
+          <Typography
+            variant="h3"
+            sx={{ fontFamily: '"Saira Semi Condensed"' }}
+          >
+            International
+          </Typography>
+          <Divider inset="none" />
+          <List size="sm" sx={{ mx: "calc(-1 * var(--ListItem-paddingX))" }}>
+            <ListItem>
+              <ListItemDecorator>
+                <Check />
+              </ListItemDecorator>
+              2 hr/week
+            </ListItem>
+            <ListItem>
+              <ListItemDecorator>
+                <Check />
+              </ListItemDecorator>
+              Financial Analytics
+            </ListItem>
+            <ListItem>
+              <ListItemDecorator>
+                <Check />
+              </ListItemDecorator>
+              Checking Account
+            </ListItem>
+            <ListItem>
+              <ListItemDecorator>
+                <Check />
+              </ListItemDecorator>
+              API Integration
+            </ListItem>
+          </List>
+          <Divider inset="none" />
+          <CardActions>
+            <Typography
+              variant="h6"
+              sx={{ fontFamily: '"Saira Semi Condensed"', mr: "auto" }}
+            >
+              $250{" "}
+              <Typography variant="body2" color="text.secondary">
+                / month
+              </Typography>
+            </Typography>
+            <Button
+              variant="soft"
+              color="neutral"
+              endIcon={<KeyboardArrowRight />}
+            >
+              Start now
+            </Button>
+          </CardActions>
+        </Card>
+      </Box>
+    </CustomBox>
+  );
+}
